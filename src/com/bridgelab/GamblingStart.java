9a27381278046c69227bd759cea6f4d886dfe218
nitis <nitish8152@gmail.com>
2022-09-27 14:40:29 +0530

    Setup Gambler Initial Values

---
+++ src/com/bridgelab/GamblingStart.java
@@ -0,0 +1,9 @@
+package com.bridgelab;
+
+public class GamblingStart {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Gambling Simulation Problem.");
+    }
+}
